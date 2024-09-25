1c6d1abe9c32599a863f54014e5765ac7c28f6eb
zamzasalim <salimzamza@gmail.com>
2024-09-25 16:17:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,13 @@
+# config.py
+import configparser
+from data import accounts
+
 url = "https://notpx.app/api/v1"
-
- # Don't Delete initData | Format: initData user= | Format: initData query=
-accounts = {
-    "account1": "initData Paste-Here",
-    "account2": "initData Paste-Here",
-    "account3": "initData Paste-Here",
-    "account4": "initData Paste-Here",
-    "account5": "initData Paste-Here",
-    "account6": "initData Paste-Here",
- # Add up to 100 accounts in this format
-}
-
-print_block = {
-    'start_x': 777,
-    'start_y': 982,
-}
-
-
-WAIT = 180 * 3
-DELAY = 0.01
-
+print_block = {'start_x': 777, 'start_y': 982}
+WAIT = 30
+DELAY = 5
 WIDTH = 1000
 HEIGHT = 1000
-MAX_HEIGHT = 50
+MAX_HEIGHT = 100
+
+# Jika ada pengaturan lain, tambahkan di sini
